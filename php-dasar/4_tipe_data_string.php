fcea21787b42dddd840eebdc3f182de70c63ed7a
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2022-04-06 09:59:30 +0700

    Tipe data string

---
+++ php-dasar/4_tipe_data_string.php
@@ -0,0 +1,27 @@
+<?php
+
+// Single Quote
+echo 'Nama: ';
+echo 'Nanda Iqbal Hanafi';
+
+// Double Quote
+echo "\n\nNama: Nanda Iqbal Hanafi\n";
+echo "Kabupaten Kulon Progo\n\n";
+
+// Heredoc
+echo <<<NANDA
+Ini adalah contoh heredoc
+Jadi kita tidak perlu enter enter manual
+bahkan kita juga bisa ngasih quote
+contohnya kayak "quote".
+NANDA;
+
+echo "\n\n";
+
+// Heredoc
+echo <<<'NANDA'
+Ini adalah contoh heredoc
+Jadi kita tidak perlu enter enter manual
+bahkan kita juga bisa ngasih quote
+contohnya kayak "quote".
+NANDA;
