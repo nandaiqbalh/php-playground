9cbc8e936e25e68791e09a4ec0c1b269edf3f335
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2022-04-07 00:03:30 +0700

    Manipulasi String

---
+++ php-dasar/15_manipulasi_string.php
@@ -0,0 +1,33 @@
+<?php
+
+// // DOT NOTATION
+// $stringSatu = "Nanda";
+// $stringDua = "Iqbal Hanafi";
+// var_dump($stringSatu . " " .  $stringDua . ". Haloo!!" . 100);
+
+// MANIPULASI STRING
+// $valueString = (string)100;
+// var_dump($valueString);
+
+// $valueInt = (int)"100";
+// var_dump($valueInt);
+
+// MENGAKSES CHAR
+// $nama = "Nanda";
+// echo $nama[0] . PHP_EOL;
+// echo $nama[1] . PHP_EOL;
+// echo $nama[2] . PHP_EOL;
+// echo $nama[3] . PHP_EOL;
+// echo $nama[4] . PHP_EOL;
+// echo $nama[5] . PHP_EOL;
+
+
+// VARIABEL PARSING
+// $name = "Nanda";
+
+// echo "Halo $name, Selamat belajar!";
+
+
+// CURLY BRACE
+$name = "Nanda";
+echo "This is {$name}'s house!";
