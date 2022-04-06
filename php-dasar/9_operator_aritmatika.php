60f2dfd0fad8151130b8c2ba4ba0390671052ed3
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2022-04-06 22:44:33 +0700

    Operator aritmatika

---
+++ php-dasar/9_operator_aritmatika.php
@@ -0,0 +1,32 @@
+<?php
+
+$a = 5;
+$b = 10;
+
+// POSITIF NEGATIF
+// $resultNegative = -$a;
+// var_dump($resultNegative);
+
+// $resultPositive = +$a;
+// var_dump($resultPositive);
+
+// // PENAMBAHAN PENGURANGAN
+// $resultTambah = $a + $b;
+// $resultKurang = $b - $a;
+// echo "Hasil tambah : " . $resultTambah . ", hasil kurang: " . $resultKurang;
+
+
+// // PERKALIAN PEMBAGIAN
+// $resultKali = $a * $b;
+// $resultBagi = $b / $a;
+// echo "Hasil kali : " . $resultKali . ", hasil bagi: " . $resultBagi;
+
+
+// // SISA BAGI
+// $resultSisaBagi = 12 % $a;
+// var_dump($resultSisaBagi);
+
+
+// PANGKAT
+$resultPangkat = $a ** 2;
+var_dump($resultPangkat);
