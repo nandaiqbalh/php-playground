d5a3b3c3a77406816af3b0a1b28b5ea5e020ce58
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2022-04-06 09:23:57 +0700

    Tipe data number

---
+++ php-dasar/2_tipe_data_number.php
@@ -0,0 +1,26 @@
+<?php
+echo "Desimal: ";
+var_dump(1234);
+
+echo "Octal: ";
+var_dump(0124);
+
+echo "Hexadesimal: ";
+var_dump(0xA2);
+
+echo "Binary: ";
+var_dump(0b01010110);
+
+echo "Undersore: ";
+var_dump(1_212_323);
+
+
+// FLOATING POINT
+echo "\n\nFloating Point: ";
+var_dump(1.234);
+
+echo "E notation plus: ";
+var_dump(1.2e3); // (1.2 x 1000)
+
+echo "E notation minus: ";
+var_dump(7e-3); // (7 x 0.001)
