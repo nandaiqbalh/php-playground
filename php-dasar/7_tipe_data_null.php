53ba9c762b7477d4c66a23f723e1270de5a63f5e
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2022-04-06 10:26:10 +0700

    Tipe data null

---
+++ php-dasar/7_tipe_data_null.php
@@ -0,0 +1,15 @@
+<?php
+$name = "Nanda";
+$name = null;
+
+$age = null;
+
+echo "Name: " . $name;
+echo "\nUmur: " . $age;
+
+// mengecek null
+echo "\nIs Name null? ";
+var_dump(is_null($age));
+
+echo "\n";
+var_dump(isset($name));
