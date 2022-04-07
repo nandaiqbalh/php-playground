660b24f4e4ae3dd9e6904b954e709646f5d37e0d
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2022-04-07 23:25:36 +0700

    Switch Case

---
+++ php-dasar/18_switch_case.php
@@ -0,0 +1,19 @@
+<?php
+$angka = 3;
+switch ($angka) {
+    case 1:
+        echo "Isi variabel angka adalah satu";
+        break;
+    case 2:
+        echo "Isi variabel angka adalah dua";
+        break;
+    case 3:
+        echo "Isi variabel angka adalah tiga";
+        break;
+    case 4:
+        echo "Isi variabel angka adalah empat";
+        break;
+    default:
+        echo "Isi variabel tidak di temukan";
+        break;
+}
