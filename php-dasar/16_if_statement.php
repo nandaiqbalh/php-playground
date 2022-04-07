64b2c785472183d15a189c6e0e61b70f7ddb365e
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2022-04-07 23:12:33 +0700

    If Statement

---
+++ php-dasar/16_if_statement.php
@@ -0,0 +1,25 @@
+<?php
+
+$a = 10;
+$b = 5;
+
+// blok kondisi (expression)
+// if ($b < $a) {
+
+//     // blok statement
+//     // hasil pengurangannya tidak negatif
+//     echo $a - $b;
+// }
+
+// blok kondisi dengan 2 kondisi
+
+$absen = 79;
+$nilai = 50;
+
+if ($absen > 75 && $nilai > 79) {
+
+    echo "\n\nAnda Lulus Sobattt!!";
+    # ...
+} else {
+    echo "Anda tidak lulus heheh";
+}
