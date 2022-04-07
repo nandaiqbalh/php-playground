3cf07695c5dfe1fff051825b0f5664b21006377c
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2022-04-07 23:18:52 +0700

    Else if statement

---
+++ php-dasar/17_else_if_statement.php
@@ -0,0 +1,10 @@
+<?php
+
+$teman = "Alfi";
+if ($teman == "Alfa") {
+    echo "Alfa adalah teman saya";
+} elseif ($teman == "Alfi") {
+    echo "Alfi adalah teman saya";
+} else {
+    echo "Saya tidak punya teman";
+}
