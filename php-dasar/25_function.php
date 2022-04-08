17f61e335d254fbb6a97052146c017ab43542849
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2022-04-08 09:21:48 +0700

    Function

---
+++ php-dasar/25_function.php
@@ -0,0 +1,24 @@
+<?php
+
+// // pembuatan fungsi
+// function sayHello()
+// {
+//     echo "Hello, Nanda!";
+// }
+
+// // pemanggilan fungsi
+// sayHello();
+
+
+$kondisi = true;
+
+if ($kondisi) {
+    function sayHello()
+    {
+        echo "Ini di dalam fungsi";
+    }
+} else {
+    // blank
+}
+
+sayHello();
