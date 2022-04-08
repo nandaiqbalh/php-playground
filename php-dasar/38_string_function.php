e7a985bb4a602eb18d9637a1f147c9b00da94a84
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2022-04-08 23:15:24 +0700

    String Function

---
+++ php-dasar/38_string_function.php
@@ -0,0 +1,12 @@
+<?php
+
+var_dump(join(",", [10, 11, 12, 13, 14]));
+
+var_dump(explode(" ", "Nanda Iqbal Hanafi"));
+
+var_dump(strtolower("NANDA IQBAL HANAFI"));
+var_dump(strtoupper("nanda iqbal hanafi"));
+
+var_dump(trim("      nanda     "));
+
+var_dump(substr("Nanda Iqbal Hanafi", 0, 5));
