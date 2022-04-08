86df191a11169524cbdee18b04e25551513aa41b
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2022-04-08 17:07:53 +0700

    Recursive Function

---
+++ php-dasar/36_recursive_function.php
@@ -0,0 +1,29 @@
+<?php
+
+// faktorial biasa
+function factorialLoop(int $value): int
+{
+    $total = 1;
+
+    for ($i = 1; $i <= $value; $i++) {
+        $total *= $i;
+    }
+
+    return $total;
+}
+
+var_dump(factorialLoop(5));
+
+
+// rekursive (memanggil dirinya sendiri)
+function factorialRecursive(int $value): int
+{
+
+    if ($value == 1) {
+        return 1;
+    } else {
+        return $value * factorialRecursive($value - 1);
+    }
+}
+
+var_dump(factorialRecursive(5));
