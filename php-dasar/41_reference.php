7e7a0d0d0c5837390e5ea495e0885e21bfd6b4dd
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2022-04-09 10:01:59 +0700

    Reference

---
+++ php-dasar/41_reference.php
@@ -0,0 +1,22 @@
+<?php
+
+
+// // ASSIGN BY REFERENCE
+// $name = "Nanda";
+
+// $otherName = &$name;
+// $otherName = "Iqbal";
+
+// echo $name . PHP_EOL;
+
+
+// // PAS BY REFERENCE
+// function increment(int &$value)
+// {
+//     $value++;
+// }
+
+// $counter = 1;
+// increment($counter);
+
+// echo $counter . PHP_EOL;
