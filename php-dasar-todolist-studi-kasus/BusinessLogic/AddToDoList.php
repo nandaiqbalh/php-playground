1df398645cb0ea7ca061ab71b879e4d37d831e66
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2022-04-09 15:58:41 +0700

    Menambah todolist

--- php-dasar-todolist-studi-kasus/BusinessLogic/AddToDoList.php
+++ php-dasar-todolist-studi-kasus/BusinessLogic/AddToDoList.php
@@ -1,8 +1,15 @@
-<?php 
+<?php
 
 /**
  * Menambahkan to do ke List
  */
-function addToDoList(){
+function addToDoList(string $inputToDo)
+{
+    global $todoList;
 
+    // mengetahui number todolist sekarang (+1 karena agar nambahinnya di 
+    // setelah data terakhir)
+    $number = sizeof($todoList) + 1;
+
+    $todoList[$number] = $inputToDo;
 }
---
+++ php-dasar-todolist-studi-kasus/Test/TestAddToDoList.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once "../php-playground/php-dasar-todolist-studi-kasus/Model/Todolist.php";
+require_once "../php-playground/php-dasar-todolist-studi-kasus/BusinessLogic/AddToDoList.php";
+
+addToDoList("Nanda");
+addToDoList("Iqbal");
+addToDoList("Hanafi");
+
+// vardum dulu untuk ngecek
+var_dump($todoList);
