dfa01b03e4238aaad69691d1d96957da1bf9ae30
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2022-04-09 10:53:44 +0700

    Create View

--- php-dasar-todolist-studi-kasus/app.php
+++ php-dasar-todolist-studi-kasus/app.php
@@ -1,10 +1,15 @@
 <?php
 
-include_once "Model/Todolist.php";
+require_once "Model/Todolist.php";
 
-// include business logic
-include_once "BusinessLogic/ShowToDoList.php";
-include_once "BusinessLogic/AddToDoList.php";
-include_once "BusinessLogic/RemoveToDoList.php";
+// reuquire business logic
+require_once "BusinessLogic/ShowToDoList.php";
+require_once "BusinessLogic/AddToDoList.php";
+require_once "BusinessLogic/RemoveToDoList.php";
+
+// require view
+require_once "View/ViewShowToDoList.php";
+require_once "View/ViewAddToDoList.php";
+require_once "View/ViewRemoveToDoList.php";
 
 echo "APP TO DO LIST" . PHP_EOL;
