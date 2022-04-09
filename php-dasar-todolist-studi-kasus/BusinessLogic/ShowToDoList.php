ac86a31ecd3ed14acc22bfaa4d68e26c164ee557
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2022-04-09 11:01:03 +0700

    Show todo list

--- php-dasar-todolist-studi-kasus/BusinessLogic/ShowToDoList.php
+++ php-dasar-todolist-studi-kasus/BusinessLogic/ShowToDoList.php
@@ -1,9 +1,21 @@
-<?php 
+<?php
 
 /**
  * Menambilkan to do yang ada pada list
  */
 
-function showToDoList(){
+function showToDoList()
+{
 
+    // Mengambil variabel array todolist dari global scope
+    // yang ada pada Model
+    global $todoList;
+
+    // judul aplikasi
+    echo "TODO LIST" . PHP_EOL;
+
+    // foreach untuk menampilkan
+    foreach ($todoList as $number => $value) {
+        echo "$number. $value" . PHP_EOL;
+    }
 }
