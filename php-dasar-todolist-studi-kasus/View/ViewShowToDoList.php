90b0f69742e3858aa9c516127f37e2ed16f9bbb2
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2022-04-09 15:27:03 +0700

    Test ShowToDoList

--- php-dasar-todolist-studi-kasus/View/ViewShowToDoList.php
+++ php-dasar-todolist-studi-kasus/View/ViewShowToDoList.php
@@ -1,5 +1,5 @@
-<?php 
+<?php
 
-function viesShowToDoList(){
-    
+function viesShowToDoList()
+{
 }
--- php-dasar-todolist-studi-kasus/app.php
+++ php-dasar-todolist-studi-kasus/app.php
@@ -1,15 +1,19 @@
 <?php
 
-require_once "Model/Todolist.php";
+require_once "../php-playground/php-dasar-todolist-studi-kasus/Model/Todolist.php";
 
 // reuquire business logic
-require_once "BusinessLogic/ShowToDoList.php";
-require_once "BusinessLogic/AddToDoList.php";
-require_once "BusinessLogic/RemoveToDoList.php";
+require_once "../php-playground/php-dasar-todolist-studi-kasus/BusinessLogic/ShowToDoList.php";
+require_once "../php-playground/php-dasar-todolist-studi-kasus/BusinessLogic/AddToDoList.php";
+require_once "../php-playground/php-dasar-todolist-studi-kasus/BusinessLogic/RemoveToDoList.php";
 
 // require view
-require_once "View/ViewShowToDoList.php";
-require_once "View/ViewAddToDoList.php";
-require_once "View/ViewRemoveToDoList.php";
+require_once "../php-playground/php-dasar-todolist-studi-kasus/View/ViewShowToDoList.php";
+require_once "../php-playground/php-dasar-todolist-studi-kasus/View/ViewAddToDoList.php";
+require_once "../php-playground/php-dasar-todolist-studi-kasus/View/ViewRemoveToDoList.php";
 
 echo "APP TO DO LIST" . PHP_EOL;
+$todoList[1] = "Belajar PHP";
+$todoList[2] = "Wawancara Momentum";
+
+showToDoList();
