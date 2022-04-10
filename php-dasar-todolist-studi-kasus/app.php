853daf25c6ea8c66a952af156093bde24e558593
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2022-04-10 13:03:06 +0700

    Finished studi kasus to do list with basic php

--- php-dasar-todolist-studi-kasus/app.php
+++ php-dasar-todolist-studi-kasus/app.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once "../php-playground/php-dasar-todolist-studi-kasus/Model/Todolist.php";
+require_once "../php-playground/php-dasar-todolist-studi-kasus/Model/Todolist.php";
 
 // reuquire business logic
 require_once "../php-playground/php-dasar-todolist-studi-kasus/BusinessLogic/ShowToDoList.php";
@@ -12,4 +13,9 @@ require_once "../php-playground/php-dasar-todolist-studi-kasus/View/ViewShowToDo
 require_once "../php-playground/php-dasar-todolist-studi-kasus/View/ViewAddToDoList.php";
 require_once "../php-playground/php-dasar-todolist-studi-kasus/View/ViewRemoveToDoList.php";
 
+// require helper
+require_once "../php-playground/php-dasar-todolist-studi-kasus/Helper/Input.php";
+
 echo "APP TO DO LIST" . PHP_EOL;
+
+viesShowToDoList();
