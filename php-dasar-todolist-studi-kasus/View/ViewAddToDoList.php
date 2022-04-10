c96c5b4c415f7294aad51a401091065a2d5f4988
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2022-04-10 12:36:22 +0700

    View Add ToDo List

--- php-dasar-todolist-studi-kasus/View/ViewAddToDoList.php
+++ php-dasar-todolist-studi-kasus/View/ViewAddToDoList.php
@@ -1,5 +1,20 @@
-<?php 
+<?php
 
-function viewAddToDoList(){
-    
+require_once "../php-playground/php-dasar-todolist-studi-kasus/Model/Todolist.php";
+require_once "../php-playground/php-dasar-todolist-studi-kasus/BusinessLogic/AddToDoList.php";
+require_once "../php-playground/php-dasar-todolist-studi-kasus/Helper/Input.php";
+
+
+function viewAddToDoList()
+{
+
+    echo "\nMENAMBAH TODO" . PHP_EOL;
+
+    $todo = input("Todo (x untuk batal)");
+
+    if ($todo == "x") {
+        // batal
+    } else {
+        addToDoList($todo);
+    }
 }
--- php-dasar-todolist-studi-kasus/View/tempCodeRunnerFile.php
+++
@@ -1,2 +0,0 @@
-<?php
-require_once "../php-playground/php-dasar-todolist-studi-kasus/Helper/Input.php";
