63d43d7b92d6899ce81c8b4d662c4405194c274e
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2022-04-11 11:10:26 +0700

    Abstract Class

---
+++ php-object-oriented-programming/15_AbstractClass.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once "../php-playground/php-object-oriented-programming/data/Location.php";
+
+use Data\{Location, City, Province, Country};
+
+// // ini akan error
+// $location = new Location;
+
+$city = new City();
+$province = new Province();
+$country = new Country();
