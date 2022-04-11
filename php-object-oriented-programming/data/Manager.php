040bb041840361390eba3b850434b7dfeddb59c1
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2022-04-11 10:49:53 +0700

    Overring constructor

--- php-object-oriented-programming/data/Manager.php
+++ php-object-oriented-programming/data/Manager.php
@@ -5,6 +5,13 @@ class Manager
 
     // membuat properties harus diawali keyword var
     var string $name;
+    var string $title;
+
+    public function __construct(string $name = "", string $title = "Manager")
+    {
+        $this->name = $name;
+        $this->title = $title;
+    }
 
     function sayHello(string $name): void
     {
@@ -14,6 +21,13 @@ class Manager
 
 class VicePresident extends Manager
 {
+
+    public function __construct(string $name = "")
+    {
+        // tidak wajib, tapi direkomendasikan
+        parent::__construct($name, "VP");
+    }
+
     function sayHello(string $name): void
     {
         echo "Hello, $name, my name is Vice President {$this->name}" . PHP_EOL;
