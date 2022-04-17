ad9a2cf3c059b50e3a35b9ae1594465620c8787a
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2022-04-17 09:38:41 +0700

    Remove file tidak digunakan

--- php-oop-todolist-studi-kasus/Test/TestInput.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-require_once "../php-playground/php-dasar-todolist-studi-kasus/Helper/Input.php";
-
-$name = input("Name");
-echo "Hello, $name";
--- php-oop-todolist-studi-kasus/Test/TestViewShowToDoList.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once "../php-playground/php-dasar-todolist-studi-kasus/View/ViewShowToDoList.php";
-require_once "../php-playground/php-dasar-todolist-studi-kasus/BusinessLogic/AddToDoList.php";
-
-addToDoList("Nanda");
-addToDoList("Iqbal");
-addToDoList("Hanafi");
-
-viesShowToDoList();
