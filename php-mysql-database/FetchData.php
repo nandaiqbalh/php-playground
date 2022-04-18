5b3360fa27a1bca468bc6ebfb6e6fc1454d15ed5
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2022-04-18 20:47:23 +0700

    Test Fetch

---
+++ php-mysql-database/FetchData.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once "../php-playground/php-mysql-database/GetConnection.php";
+
+$connection = getConnection();
+
+$username = "admin'; #";
+$password = "admin";
+
+$sql = "SELECT * FROM admin WHERE username = :username AND password = :password";
+$statement = $connection->prepare($sql);
+
+$statement->bindParam("username", $username);
+$statement->bindParam("password", $password);
+$statement->execute();
+
+if($row = $statement->fetch()){
+    echo "Sukses Login " . $row["username"] . PHP_EOL;
+} else{
+    echo "Gagal Login" . PHP_EOL;
+}
+
+$connection = null;
