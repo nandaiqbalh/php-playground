2018eb5be764ab7c80113c237c8deb848f5439d4
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2022-05-09 14:19:27 +0700

    Membuat project composer

---
+++ php-composer/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '90055c07f2d2097c7e548c3d0c9c2daa160f7a34',
+        'name' => 'nandaiqbalh/php-composer',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'nandaiqbalh/php-composer' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '90055c07f2d2097c7e548c3d0c9c2daa160f7a34',
+            'dev_requirement' => false,
+        ),
+    ),
+);
